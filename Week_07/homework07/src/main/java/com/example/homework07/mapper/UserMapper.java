fc6e871f5a309ee311ec9d90da20002fe97ccb1f
marbleG <1218892028@qq.com>
2021-05-04 22:38:13 +0800

    提交动态数据源1.0

---
+++ Week_07/homework07/src/main/java/com/example/homework07/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.example.homework07.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.homework07.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserMapper extends BaseMapper<User> {
+
+}
